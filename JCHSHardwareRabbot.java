2351f02f9b6d4d9f8f99662e7388b984fe9bafae
JCHSRobotics <JCHSRoboticsEngineering@gmail.com>
2020-11-19 16:17:42 -0800

    Testing commit - jjh

--- JCHSHardwareRabbot.java
+++ JCHSHardwareRabbot.java
@@ -52,6 +52,14 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  */
 public class JCHSHardwareRabbot
 {
+    // delete this line
+    // delete this line// delete this line
+    // delete this line
+    // delete this line
+    // delete this line
+    // delete this line
+    // delete this line
+    // delete this line TODO
     /* Public OpMode members. */
     public DcMotor  leftDrive   = null;
     public DcMotor  rightDrive  = null;

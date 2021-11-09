64fc86856b07ee00c5c8da708e35ea28e53b3498
JCHSRobotics <jchsroboticsengineering@gmail.com>
2021-11-09 13:18:27 -0800

    2020 Webcam

--- JCHSRabbotAutonomous.java
+++ JCHSRabbotAutonomous.java
@@ -61,7 +61,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@Autonomous(name="Pushbot: Auto Drive By Encoder", group="Pushbot")
+@Autonomous(name="Rabbot: Chassis Auto Drive", group="Rabbot")
 // @Disabled
 public class JCHSRabbotAutonomous extends LinearOpMode {
 
@@ -122,7 +122,7 @@ public class JCHSRabbotAutonomous extends LinearOpMode {
 
         // Step through each leg of the path,
         // Note: Reverse movement is obtained by setting a negative distance (not speed)
-        encoderDrive(DRIVE_SPEED,  48,  48, 5.0);  // S1: Forward 47 Inches with 5 Sec timeout
+        encoderDrive(DRIVE_SPEED,  1000,  1000, 10.0);  // S1: Forward 47 Inches with 5 Sec timeout
         // encoderDrive(TURN_SPEED,   12, -12, 4.0);  // S2: Turn Right 12 Inches with 4 Sec timeout
         // encoderDrive(DRIVE_SPEED, -24, -24, 4.0);  // S3: Reverse 24 Inches with 4 Sec timeout
 
